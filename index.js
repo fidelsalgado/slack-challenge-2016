b1f91ed8ea83a5b7667bc8acd338ddd504e030e0
Fidel Salgado <fidels@stanford.edu>
2016-12-24 17:05:53 -0600

    added docs to each helper function

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const SLACK_TOKEN = 'rFfHJmoHxZSaPi302yac7yOM';
 const TEAM_ID = 'T3F7VCUQ1';
 const tttGame = require('./lib/ttt-game.js');
-const slackInputParser = require('./lib/slack-input-parser');
 let gamesOn = {};
 
 const express = require('express'); 
@@ -13,10 +12,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.set('port', (process.env.PORT || 5000));
 
+/**
+ * Endpoint that handles POST request from slack. It makes sure that the request
+ * has a valid format, and it keeps track of a game per channel.
+ */
 app.post('/', (req, res) => { 
-  // TODO: Check that token and team is valid
-  // TODO: Check if body is valid
-
   let token = req.body.token;
   let team = req.body.team_id;
   if(token !== SLACK_TOKEN || team !== TEAM_ID) {
@@ -37,6 +37,9 @@ app.post('/', (req, res) => {
   res.json(gamesOn[channel].handleUserInput(text, username));
 });
 
+/**
+ * Any other request will not be found.
+ */
 app.all('/', (req, res) => { 
   res.status(404).send('Not found');
 });
