a9b739b39b2e7920413081ea32b0db998b682d45
Fidel Salgado <fidels@stanford.edu>
2016-12-24 15:18:44 -0600

    handling multiple channels

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict'; 
 const SLACK_TOKEN = 'rFfHJmoHxZSaPi302yac7yOM';
-const tttGame = require('./lib/ttt-game.js')();
+const tttGame = require('./lib/ttt-game.js');
 const slackInputParser = require('./lib/slack-input-parser');
+let gamesOn = {};
 
 const express = require('express'); 
 const bodyParser = require('body-parser'); 
@@ -13,10 +14,21 @@ app.set('port', (process.env.PORT || 5000));
 
 app.post('/', (req, res) => { 
   // TODO: Check that token and team is valid
-  res.json(tttGame.handleUserInput(req.body.text, req.body.user_name));
+  // TODO: Check if body is valid
+
+  let text = req.body.text;
+  let username = req.body.user_name;
+  let channel = req.body.channel_id;
+
+  if(!(channel in gamesOn)) {
+    gamesOn[channel] = tttGame();
+  }
+
+  res.json(gamesOn[channel].handleUserInput(text, username));
 });
 
 app.all('/', (req, res) => { 
+  // TODO: Better handling
   res.status(404).send('Not found');
 });
 
